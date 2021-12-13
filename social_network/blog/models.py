d1567567bc7a52074f7dd90f55f8d9bd1b7f4a6f
mitrandir-pl <leshkopavel@gmail.com>
2021-12-13 14:16:05 +0300

    userapi model added

--- social_network/blog/models.py
+++ social_network/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserAPI(models.Model):
+    nickname = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    role = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nickname
