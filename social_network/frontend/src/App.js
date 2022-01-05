87b6bc758857cf4ab2f0d1a84ad7bc9e5a30f514
mitrandir-pl <leshkopavel@gmail.com>
2022-01-05 12:46:06 +0300

    comment model changed

--- social_network/frontend/src/App.js
+++ social_network/frontend/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Users from './components/Users'
 import News from './components/News'
+import Comments from './components/Comments';
 
 function App() {
-
+  const [posts, setPosts] = useState([
+        {username: 'user1', email: 'aadada'},
+        {username: 'user2', email: 'bbbbb'},
+        {username: 'user3', email: 'ccccc'}
+      ]
+  )
   return (
       <div>
         <Users />
         <News />
+        <Comments />
       </div>
+      // <div>
+      //     {posts.map((post) =>
+      //         <h1 key={post.username}>Hello</h1>
+      //     )}
+      // </div>
   );
 }
 
