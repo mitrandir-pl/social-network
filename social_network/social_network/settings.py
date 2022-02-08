1e5a30a84bcc5a0becdf4ba53531a8e9c91d3fcd
mitrandir-pl <leshkopavel@gmail.com>
2022-02-08 12:11:31 +0300

    drf-yasg

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
+    # 'django.contrib.staticfiles',
+    'drf_yasg',
     # my apps
     'blog',
     'user',
--- social_network/social_network/urls.py
+++ social_network/social_network/urls.py
@@ -6,6 +6,24 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny, ),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +32,7 @@ urlpatterns = [
     path('api/v1/comments/', include('comments.urls'), name='comments'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
